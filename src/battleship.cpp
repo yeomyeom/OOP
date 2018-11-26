8ed5b061de451596f01421b94fd6f6f266807663
yeomyeom <ysy9639@naver.com>
2018-11-26 21:17:38 +0900

    Add Bots!

--- src/battleship.cpp
+++ src/battleship.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib,"ws2_32.lib")
+#include <WinSock2.h>
 #include <Windows.h>
 #include <stdio.h>
 #include <vector>
@@ -266,7 +269,8 @@ private:
 public:
 	vector<ship> ship_v;
 	vector<ship>::iterator ship_index;
-	player(int id_, int bot_num_) : id(id_), bot_num(bot_num_), unit_num(0){
+	int state;//멀티에서 쓸거
+	player(int id_, int bot_num_) : id(id_), bot_num(bot_num_), unit_num(0), state(0){
 		//unit_max = MAPSIZE * MAPSIZE * 0.04;// 배를 최대로 만들 수 있는 수를 어떻게 해야할까?
 		unit_max = 4;//TEST 코드에서는 4대를 최대 보유수로 함
 		for (int i = 0;i < MAPSIZE;i++)
@@ -313,13 +317,13 @@ public:
 				y = rand() % MAPSIZE;
 				Sleep(100);
 				c = rand() % 4;
-				cout << x << y<< c<<"배 만들기 시도" << endl;
+				//cout << x << y<< c<<"배 만들기 시도" << endl;
 			} while (map_check(map, x, y, c, health) != 1);
 			unit_num++;
 			ship s_new(name, kind, x, y, c, health, unit_num);
 			ship_v.push_back(s_new);
 			//unit_index = unit_num;//unit_index 변수는 아직 무슨 역할에 넣을지 모름
-			cout << unit_num << " 번째 배를 생성했습니다." << endl;
+			//cout << unit_num << " 번째 배를 생성했습니다." << endl;
 			drawship(s_new);
 		}
 		else
@@ -609,9 +613,15 @@ void gamestart_single()
 	{
 		if (alive == 1)//단 한명만 살아남으면 승리
 		{
-			cout << "P" << p_index->getid() << " 가 승리했습니다." << endl;
+			for (p_index = p_v.begin();p_index != p_v.end();p_index++) {
+				if (p_index->ship_v[0].gethealth() != 0)
+				{
+					cout << "P" << p_index->getid() << " 가 승리했습니다." << endl;
+				}
+			}
 			return;
 		}
+		alive = bot + 1;
 		for (p_index=p_v.begin();p_index != p_v.end();p_index++) //모든 플레이어가 완료할때까지 반복
 		{
 			who = who % (bot + 1);
@@ -632,66 +642,98 @@ void gamestart_single()
 					for (tmp = 0;tmp < p_index->get_unitmax();tmp++) {
 						p_index->drawship(p_index->ship_v[tmp]);//격침당한 배는 빼고 맵에 그림 ship 객체는 안지움 vector에 저장되서 삭제하려면 앞에 있는 애들 다 시 당겨 줘야함
 					}
-					for (i = 0;i <= bot;i++)
+					if (who == 0) 
 					{
-						cout << "P" << i << endl;
-						cout << "=======================" << endl;
-						maps_v[i].showothermap();
-						cout << "=======================" << endl;
-					}
-					cout << "P" << who << endl;
-					cout << "=======================" << endl;
-					p_index->showmap();
-					cout << "=======================" << endl;
-					p_index->showship();
-					if (p_index->ship_index->gethealth() > 0) {//,배가 살아있는가?
-						cout << "어느 동작을 할 것입니까? 1 : 공격  2. 이동  다른값. 강제 종료" << endl;
-						cin >> input;
-						if (input == 1)
+						for (i = 0;i <= bot;i++)
 						{
-							do {
-								cout << "공격할 플레이어는? (최대) " << bot << endl;
-								cin >> p;
-							} while (p > bot || p < 0);
-							do {
-								cout << "공격할 좌표를 입력하시오 : ";
-								cin >> xpos;
-								cin >> ypos;
-							} while ((xpos > MAPSIZE || xpos <0) && (ypos > MAPSIZE || ypos < 0));
-							tmp = p_index->attack(xpos, ypos, &p_v[p]); //p번째에 있는 플레이어 공격 p_v는 1부터 시작이 아니라 0부터 시작이다.
-							if (tmp != 0)
-							{
-								maps_v[p].other_map[ypos][xpos] = 1;
-							}
+							cout << "P" << i << endl;
+							cout << "=======================" << endl;
+							maps_v[i].showothermap();
+							cout << "=======================" << endl;
 						}
-						else if (input == 2)
-						{
-							do {
-								cout << p_index->ship_index->getmove_point() << " 만큼 0 : 좌회전 1 :직진 2 : 우회전" << endl;
-								cin >> input;
-							} while (input != 0 && input != 1 && input != 2);
-							if (p_index->move(input, *(p_index->ship_index)) == 1)
+						cout << "P" << who << endl;
+						cout << "=======================" << endl;
+						p_index->showmap();
+						cout << "=======================" << endl;
+						p_index->showship();
+						if (p_index->ship_index->gethealth() > 0) {//,배가 살아있는가?
+							cout << "어느 동작을 할 것입니까? 1 : 공격  2. 이동  다른값. 강제 종료" << endl;
+							cin >> input;
+							if (input == 1)
 							{
-								cout << "이동했습니다." << endl;
+								do {
+									cout << "공격할 플레이어는? (최대) " << bot << endl;
+									cin >> p;
+								} while (p > bot || p < 0);
+								do {
+									cout << "공격할 좌표를 입력하시오 : ";
+									cin >> xpos;
+									cin >> ypos;
+								} while ((xpos > MAPSIZE || xpos < 0) && (ypos > MAPSIZE || ypos < 0));
+								tmp = p_index->attack(xpos, ypos, &p_v[p]); //p번째에 있는 플레이어 공격 p_v는 1부터 시작이 아니라 0부터 시작이다.
+								if (tmp != 0)
+								{
+									maps_v[p].other_map[ypos][xpos] = 1;
+								}
+							}
+							else if (input == 2)
+							{
+								do {
+									cout << p_index->ship_index->getmove_point() << " 만큼 0 : 좌회전 1 :직진 2 : 우회전" << endl;
+									cin >> input;
+								} while (input != 0 && input != 1 && input != 2);
+								if (p_index->move(input, *(p_index->ship_index)) == 1)
+								{
+									cout << "이동했습니다." << endl;
+								}
+								else
+								{
+									cout << "값이 이상합니다." << endl;
+								}
 							}
 							else
 							{
-								cout << "값이 이상합니다." << endl;
+								return;//강제 종료
 							}
 						}
 						else
 						{
-							return;//강제 종료
+							cout << "격침된 배입니다." << endl;
 						}
+						Sleep(1000);
+						system("cls");
+						cout << "////////////////다음 배//////////////" << endl;
 					}
-					else
+					else//bot
 					{
-						cout << "격침된 배입니다." << endl;
+						system("cls");
+						cout << who <<" bot 진행 중입니다." << endl;
+						if (p_index->ship_index->gethealth() > 0) {//,배가 살아있는가?
+							input = rand() % 2;
+							if (input == 1)
+							{
+								xpos = rand()%MAPSIZE;
+								Sleep(1000);
+								ypos = rand()%MAPSIZE;
+								Sleep(1000);
+								p = rand() % (bot + 1);
+								tmp = p_index->attack(xpos, ypos, &p_v[p]);
+								if (tmp != 0)
+								{
+									maps_v[p].other_map[ypos][xpos] = 1;
+								}
+							}
+							else
+							{
+								cout << "이동중입니다." << endl;
+								Sleep(1000);
+								input = rand() % 3;
+								p_index->move(input, *(p_index->ship_index));
+							}
+						}
+						system("cls");
 					}
-					Sleep(1000);
-					system("cls");
-					cout << "////////////////다음 배//////////////" << endl;
-				}
+				}	
 			}
 			who++;
 			cout << p_index->getid() << " 번째 플레이어 차례가 종료되었습니다." << endl;
@@ -700,8 +742,6 @@ void gamestart_single()
 	}
 	//50턴이 끝나고 나면 누가 이겼는지는 안나옴
 	cout << "50턴이 넘었습니다. 경기를 종료합니다." << endl;
-
-
 	//4. 반복시작
 	//   1. ship vector에 1번째 있는 배를 움직일 것인가 공격할 것인가
 	//   2. 움직이면 방향(0왼 1직진 2오), 공격할꺼면 공격 위치와 공격할 플레이어(x,y, player)
@@ -714,32 +754,146 @@ void gamestart_multi()
 	int turn = 0;
 	int input, p, tmp, xpos, ypos, i;
 	int bot;
-	cout << "multi play" << endl;
-	cout << "Map Size " << MAPSIZE << endl;
-	cout << "접속 대기중 입니다. : ";
-	vector<player> p_v;
-	vector<player>::iterator p_index;
-	vector<othermap> maps_v;//맞았는지 안맞았는지 보여주는 map vector 다른사람이 쏘는 것도 볼 수 있다.
-	vector<othermap> ::iterator maps_index;
+	char ip;
+	cout << "multi play 아이피 주소를 확인하세요" << endl;
+	system("ipconfig");
+	cout << "연결을 위한 IP를 입력하세요(포트는 50000으로 고정입니다.)" << endl;
+	cin >> ip;
+	WSADATA wsadata;
+	SOCKET hservsocket;
+	SOCKET hclntsocket;
+	SOCKADDR_IN servaddr;
+	SOCKADDR_IN clntaddr;
+	int sizeclntaddr;
 
-	for (int i = 0;i <= 2;i++)//0=본인 1=상대방
+	if (WSAStartup(MAKEWORD(2, 2), &wsadata))
 	{
-		player p(i, 2);
-		p_v.push_back(p);
-		for (int j = 0;j < p_v[i].get_unitmax();j++)
-		{
-			p_v[i].makeship("전함", 2, 4);//나중에 ship 상속받은 ship_항공모함, ship_전함, ship_구축함이 들어갈 자리
-		}
-		othermap map;
-		maps_v.push_back(map);//상대방이 사격했을때 해당 위치에 무엇이 있는지 알기 위한 빈 2차원 배열
+		cout << "WSAStartup fuc error" << endl;
 	}
 
+	hservsocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hservsocket == INVALID_SOCKET)
+	{
+		cout << "socket fuc error" << endl;
+	}
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(atoi("50000"));
+	//서버 생성
+	//클라이언트 생성
+	cout << "Map Size " << MAPSIZE << endl;
+	vector<othermap> maps_v;//맞았는지 안맞았는지 보여주는 map vector 다른사람이 쏘는 것도 볼 수 있다.
+	vector<othermap> ::iterator maps_index;
+	int id;
+	cout << "id를 입력하세요 : ";
+	cin >> id;
+	player player(id, 2);
+	for (int j = 0;j < player.get_unitmax();j++)
+	{
+		player.makeship("전함", 2, 4);//나중에 ship 상속받은 ship_항공모함, ship_전함, ship_구축함이 들어갈 자리
+	}
+	othermap map;
+	maps_v.push_back(map);//상대방이 사격했을때 해당 위치에 무엇이 있는지 알기 위한 빈 2차원 배열
+	maps_v.push_back(map);//내꺼
+	while (turn < 50)
+	{
 
+	}
 	//1. 멀티플레이
 	//2. 멀티는 1:1
 	//3. 플레이어 순서는 id 오름차순으로 정해짐
 	//4. 반복시작
+	WSACleanup();
 }
+void server(char *message)
+{
+	WSADATA wsadata;
+	SOCKET hservsocket;
+	SOCKET hclntsocket;
+	SOCKADDR_IN servaddr;
+	SOCKADDR_IN clntaddr;
+	int sizeclntaddr;
+
+	/*Load Winsock 2.2 DLL*/
+	if (WSAStartup(MAKEWORD(2, 2), &wsadata))
+	{
+		cout << "WSAStartup fuc error" << endl;
+	}
+
+	hservsocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hservsocket == INVALID_SOCKET)
+	{
+		cout << "socket fuc error" << endl;
+	}
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(atoi("50000"));
+
+	if (bind(hservsocket, (SOCKADDR*)&servaddr, sizeof(servaddr)) == SOCKET_ERROR)//소켓 주소 할당
+	{
+		cout << "bind fuc error" << endl;
+	}
+
+	if (listen(hservsocket, 5) == SOCKET_ERROR)//연결 요청 대기 5회
+	{
+		cout << "listen fuc error" << endl;
+	}
+	sizeclntaddr = sizeof(clntaddr);
+	hclntsocket = accept(hservsocket, (SOCKADDR *)&clntaddr, &sizeclntaddr);//연결 요청 수락
+
+	if (hclntsocket == INVALID_SOCKET)
+	{
+		cout << "accept fuc error" << endl;
+	}
+	send(hclntsocket, message, sizeof(message), 0);//데이터 전송
+
+	closesocket(hclntsocket);
+	WSACleanup();
+}
+
+void client(const char *ip, char *receive)
+{
+	WSADATA wsadata;
+	SOCKET hsocket;
+	char message[MAPSIZE*MAPSIZE];
+	int strlen;
+	SOCKADDR_IN servaddr;
+
+	//Load Winsocket 2.2 DLL
+	if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0)
+	{
+		cout << "WSAStartup fuc error" << endl;
+	}
+	hsocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hsocket == INVALID_SOCKET)
+	{
+		cout << "hsocket fuc error" << endl;
+	}
+
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr(ip);
+	servaddr.sin_port = htons(atoi("50000"));
+
+	if (connect(hsocket, (SOCKADDR*)&servaddr, sizeof(servaddr)) == SOCKET_ERROR)
+	{
+		cout << "connect fuc error" << endl;
+	}
+	strlen = recv(hsocket, message, sizeof(message) - 1, 0);
+	if (strlen == -1)
+	{
+		cout << "read fuc error" << endl;
+	}
+	message[strlen] = 0;
+	strcmp(receive, message);
+	//cout << "Message from server : " << message << endl;
+	closesocket(hsocket);
+	WSACleanup();
+}
+
+
 int main()
 {
 	/*player p1(1);
@@ -755,6 +909,7 @@ int main()
 	p2.showmap();
 	p2.showship();*/
 	//system("mode con cols=75 lines=60");
+	system("pause");
 	gamestart_single();
 
 	system("pause");
